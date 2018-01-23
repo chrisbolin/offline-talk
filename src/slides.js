ccdf2ffdce7777e70ed1c45219ce8b859c745b47
Chris Bolin <bolin.chris@gmail.com>
2018-01-22 18:02:57 -0700

    prettier. boulder

--- src/slides.js
+++ src/slides.js
@@ -1,4 +1,5 @@
 export default `
+
 # ✨Offline Only✨
 
 ### _a weird project I made and what it taught me_
@@ -7,19 +8,19 @@ Chris Bolin, [Formidable](https://formidable.com)
 
 [@bolinchris](https://twitter.com/bolinchris)
 
-React Denver, October 2017
+Boulder React Meetup, January 2018
 
 ---
 
 # What to Expect ✅
 
-- Browsers' Offline API
-- Service Workers
-- Open sourcing your work
-- Promoting a project
-- Preparing for the best
-- Bracing for the worst
-- Internationalization
+* Browsers' Offline API
+* Service Workers
+* Open sourcing your work
+* Promoting a project
+* Preparing for the best
+* Bracing for the worst
+* Internationalization
 
 ---
 
@@ -31,10 +32,11 @@ React Denver, October 2017
 
 # Offline API 🔌
 
-- Monitor the state of the device's connection
-- \`navigator.onLine\`
-- \`online\` and \`offline\` [events](https://developer.mozilla.org/en-US/docs/Online_and_offline_events)
-- Broad support - 94% worldwide traffic, 98% US
+* Monitor the state of the device's connection
+* Uses: notify user if offline; send data when connected
+* \`navigator.onLine\`
+* \`online\` and \`offline\` [events](https://developer.mozilla.org/en-US/docs/Online_and_offline_events)
+* Broad support - 94% worldwide traffic, 98% US
 
 (but there are always exceptions)
 
@@ -42,27 +44,27 @@ React Denver, October 2017
 
 # Service Workers 🤖
 
-- Workers are background scripts
-- Allow applications to be available offline
-- Acts as a proxy, controlling how requests are handled
-- [\`create-react-app\`](https://github.com/facebookincubator/create-react-app) projects include a worker
+* Workers are background scripts
+* Allow applications to be available offline
+* Acts as a proxy, controlling how requests are handled
+* [\`create-react-app\`](https://github.com/facebookincubator/create-react-app) projects include a worker
 
 ---
 
 # Open Source Your Work 📖
 
-- Sign of good faith
-- Provides a convenient answer to point to
-- Reusable, general-interest code? Make a library
-- I open-sourced both the website and a library
-- Library is simple at first; will ideally grow
+* Sign of good faith
+* Easy answer to "how does it work?"
+* Reusable, general-interest code? Make a library
+* But you can open-source a website, too!
+* I open-sourced both the website and a library
 
 ---
 
 # Simple React Library ⚛
 
-- Published [react-detect-offline](https://github.com/chrisbolin/react-detect-offline)
-- React libraries should ideally be *declarative*
+* Published [react-detect-offline](https://github.com/chrisbolin/react-detect-offline)
+* React libraries should ideally be _declarative_
 
 ![react-detect-offline](./react-detect-offline.png)
 
@@ -70,12 +72,12 @@ React Denver, October 2017
 
 # Promoting a Project 📰
 
-- Find a popular friend to post or retweet
-- Tag the technologies you used
-- Engage with others
-- Understand the specific platforms
-- Press will usually reach largest audience
-- Add Open Graph tags for social media previews
+* Find a popular friend to post or retweet
+* Tag the technologies you used
+* Engage with others
+* Understand the specific platforms
+* Press will usually reach largest audience
+* Add Open Graph tags for social media previews
 
 ---
 
@@ -84,6 +86,7 @@ React Denver, October 2017
 [Meta tags](http://ogp.me/) that provide previews for Twitter, FB, Slack...
 
 \`\`\`
+
 <meta property="og:title" content="Offline Only" />
 og:description "Disconnection can be good."
 og:url "https://chris.bolin.co/offline/"
@@ -96,9 +99,9 @@ og:image "https://chris.bolin.co/offline/preview.png"
 
 # Twitter 🐤
 
-- Use [TweetDeck](https://tweetdeck.twitter.com)
-- Search for your URL
-- Reach out if someone posts your project
+* Use [TweetDeck](https://tweetdeck.twitter.com)
+* Search for your URL
+* Reach out if someone posts your project
 
 ---
 
@@ -106,10 +109,10 @@ og:image "https://chris.bolin.co/offline/preview.png"
 
 ![ga](./ga.png)
 
-- Are you ready if things go well?
-- CDNs - domestic & international. (I use [Cloudflare](https://www.cloudflare.com))
-- HTTPS / SSL
-- Scaling servers and databases
+* Are you ready if things go well?
+* CDNs - domestic & international. (I use [Cloudflare](https://www.cloudflare.com))
+* HTTPS / SSL
+* Scaling servers and databases
 
 ---
 
@@ -117,15 +120,16 @@ og:image "https://chris.bolin.co/offline/preview.png"
 
 ![reddit](./reddit.png)
 
-- Trolls are real
-- Whatever you make, they'll try to berate or break it
-- Try joking or ignoring. Don't be afraid to mute.
-- *Usually supportive:* Twitter, Facebook, LinkedIn
-- *Sometimes caustic:* r/programming, Hacker News
+* Trolls are real
+* Whatever you make, they'll try to berate or break it
+* Try joking or ignoring. Don't be afraid to mute.
+* _Usually supportive:_ Twitter, Facebook, LinkedIn
+* _Sometimes caustic:_ r/programming, Hacker News
 
 ---
 
 ## If you build it
+
 ## they will break it 💔
 
 ![skycoins](./skycoins-before.png) ![skycoins](./skycoins-after.jpg)
@@ -134,11 +138,11 @@ og:image "https://chris.bolin.co/offline/preview.png"
 
 # Internationalization 🌍
 
-- People might volunteer to translate your work
-- Translation is hard; proofreading is hard
-- Small features help: autodetect, sharable links
-- Multi-language code can be complex
-- _...but you can reach a whole new audience_
+* People might volunteer to translate your work
+* Translation is hard; proofreading is hard
+* Small features help: autodetect, sharable links
+* Multi-language code can be complex
+* _...but you can reach a whole new audience_
 
 ![translation](./translation.gif)
 
@@ -146,21 +150,21 @@ og:image "https://chris.bolin.co/offline/preview.png"
 
 # Editing Foreign Text 📝
 
-- Beware of autocorrect
-- Google Translate is your friend
-- Editing right-to-left text is very hard
+* Beware of autocorrect
+* Google Translate is your friend
+* Editing right-to-left text is very hard
 
 ---
 
 # Takeaways 🍱
 
-- The web is an amazing place for experimentation
-- Use \`create-react-app\` (especially on small projects)
-- Usually no one will care about your work
-- But be prepared: use Open Graph, HTTPS, and CDNs
-- Trolls are real, so choose your battles wisely
-- Internationalization is hard, but can open doors
-- **Have fun, darnit**
+* The web is an amazing place for experimentation
+* Use \`create-react-app\` (especially on small projects)
+* Usually no one will care about your work
+* But be prepared: use Open Graph, HTTPS, and CDNs
+* Trolls are real, so choose your battles wisely
+* Internationalization is hard, but can open doors
+* **Make stuff. Have fun.**
 
 ---
 
